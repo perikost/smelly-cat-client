3b9e1e45565d222d66508ef565464c5161432e5e
Periklis Kostamis <perikliskostamis@gmail.com>
2024-02-13 14:51:26 +0200

    Add dummy components

---
+++ src/app/adopt/adopt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adopt',
+  standalone: true,
+  imports: [],
+  templateUrl: './adopt.component.html',
+  styleUrl: './adopt.component.scss'
+})
+export class AdoptComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,29 @@
 import { Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
+import { AdoptComponent } from './adopt/adopt.component';
+import { StoriesComponent } from './stories/stories.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
 
 export const routes: Routes = [
     { 
         path: '',
         component: ContactComponent
+    },
+    {
+        path: 'adopt',
+        component: AdoptComponent
+    },
+    {
+        path: 'stories',
+        component: StoriesComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'blog',
+        component: BlogComponent
     }
 ];
